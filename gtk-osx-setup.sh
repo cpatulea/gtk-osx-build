604dc68ab17931a9e3ed4f059c291b0b00b691bd
totaam <antoine@xpra.org>
2022-02-17 11:49:05 +0700

    merge all from upstream (just copy over)

--- gtk-osx-setup.sh
+++ gtk-osx-setup.sh
@@ -57,11 +57,8 @@ envvar DEV_SRC_ROOT "$DEVROOT/Source"
 envvar PYENV_INSTALL_ROOT "$DEV_SRC_ROOT/pyenv"
 envvar PYENV_ROOT "$DEVPREFIX/share/pyenv"
 envvar PIP_CONFIG_DIR "$HOME/.config/pip"
-envvar CARGO_HOME "$DEVPREFIX"
-envvar RUSTUP_HOME "$DEVPREFIX"
 
 export PYTHONWARNINGS=ignore:DEPRECATION::pip._internal.cli.base_command
-export PYTHONPATH="$PYTHONUSERBASE/lib/python/site-packages":"$PYTHONPATH"
 
 if test ! -d "$DEVPREFIX/bin" ; then
     mkdir -p "$DEVPREFIX/bin"
@@ -83,49 +80,42 @@ if test ! -x "$PYENV_INSTALL_ROOT/libexec/pyenv"; then
   git clone $GITHUB/pyenv/pyenv.git "$PYENV_INSTALL_ROOT"
 else
     pushd "$PYENV_INSTALL_ROOT"
-    git pull
+    git pull --ff-only
     popd
 fi
 
-if test ! -x "$DEVPREFIX/bin/pyenv" ; then
+PYENV="$DEVPREFIX/bin/pyenv"
+
+if test ! -x "$PYENV" ; then
     ln -s "$PYENV_INSTALL_ROOT/bin/pyenv" "$DEVPREFIX/bin"
 fi
 
-# Setup PIP; note that we're assuming that python is the system python
-# at this point. Having set $PYTHONUSERBASE, pip will be installed in
-# $PYTHONUSERBASE/bin and the requisite modules will go in
-# $PYTHONUSERBASE/lib/python/site-packages.
+#Force installation and we hope use of Python with PyEnv. We must
+#avoid using the Apple-provided Python2 because jhbuild doesn't work
+#with python2 any more, and the Apple-provided python3 because it
+#doesn't include a usable libpython for libxml2 to link against.
 
-if test ! -f "`eval echo $PIP_CONFIG_FILE`" ; then
-    export PIP_CONFIG_FILE="$PIP_CONFIG_DIR/pip.conf"
-    mkdir -p "$PIP_CONFIG_DIR"
-fi
-PIP=`which pip`
-if test ! -x "`eval echo $PIP`" ; then
-    mv=`python --version 2>&1 | cut -b 12-13`
-    if test $mv -lt 11 ; then
-        curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o "$DEVPREFIX/get-pip.py"
-        python "$DEVPREFIX/get-pip.py" --user
-        rm "$DEVPREFIX/get-pip.py"
-    else
-        python -m ensurepip --user
-    fi
-    PIP="$PYTHONUSERBASE/bin/pip"
-fi
+export PYTHON_CONFIGURE_OPTS="--enable-shared"
+export PYENV_PYTHON_VERSION=3.10.0
+$PYENV install -v $PYENV_PYTHON_VERSION
+PIP="$PYENV_ROOT/shims/pip3"
+
 $PIP install --upgrade --user pip
 
 # Install pipenv
 $PIP install --upgrade --user pipenv==2020.11.15
-pip_remove typing
 PIPENV="$PYTHONUSERBASE/bin/pipenv"
+export WORKON_HOME=$DEVPREFIX/share/virtualenvs
 
+JHBUILD_RELEASE_VERSION=3.38.0
 # Install jhbuild
 if test ! -d "$DEV_SRC_ROOT/jhbuild/.git" ; then
-    git clone -b 3.36.0 $GITLAB/jhbuild.git "$DEV_SRC_ROOT/jhbuild"
+    git clone -b $JHBUILD_RELEASE_VERSION $GITLAB/jhbuild.git "$DEV_SRC_ROOT/jhbuild"
     cd "$DEV_SRC_ROOT/jhbuild"
 else #Get the latest if it's already installed
     cd "$DEV_SRC_ROOT/jhbuild"
-    git reset --hard 3.38.0
+    git pull
+    git reset --hard $JHBUILD_RELEASE_VERSION
 fi
 
 # Install Ninja
@@ -138,14 +128,35 @@ fi
 
 #Install Rust (required for librsvg, which gtk needs to render its icons.)
 RUSTUP=`which rustup`
-if test ! -x "$RUSTUP" -a ! -x "$DEVPREFIX/bin/rustup"; then
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
+if test -x "$RUSTUP"; then
+    case `dirname "$RUSTUP"` in
+        "$DEVPREFIX"*)
+            DEFAULT_CARGO_HOME=$(dirname $(dirname "$RUSTUP"))
+            envvar CARGO_HOME "$DEFAULT_CARGO_HOME"
+            CARGO_HOME_DIR=$(basename "$CARGO_HOME")
+            if test "$CARGO_HOME_DIR" == "cargo"; then
+                CARGO_HOME_BASE=$(dirname "$CARGO_HOME")
+                envvar RUSTUP_HOME "$CARGO_HOME_BASE"/rustup
+            else
+                envvar RUSTUP_HOME "$CARGO_HOME"
+            fi
+            ;;
+        *)
+            envvar CARGO_HOME "$HOME/.cargo"
+            envvar RUSTUP_HOME "$HOME/.rustup"
+            ;;
+    esac
+else
+    envvar CARGO_HOME "$DEVPREFIX"
+    envvar RUSTUP_HOME "$DEVPREFIX"
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --no-modify-path 
 fi
 
 if test ! -d "$DEVPREFIX/etc" ; then
     mkdir -p "$DEVPREFIX/etc"
 fi
 
+PYENV_MINOR_VERSION=$(echo $PYENV_PYTHON_VERSION | cut -d . -f 1,2)
 cat  <<EOF > "$DEVPREFIX/etc/Pipfile"
 [[source]]
 url = "https://pypi.python.org/simple"
@@ -159,7 +170,7 @@ meson = {version=">=0.56.0"}
 jhbuild = "$DEVPREFIX/libexec/run_jhbuild.py"
 
 [requires]
-python_version = "3.8"
+python_version = "$PYENV_MINOR_VERSION"
 EOF
 cat <<EOF > "$DEVPREFIX/etc/pipenv-env"
 export PYTHONUSERBASE="$PYTHONUSERBASE"
@@ -173,11 +184,15 @@ cat <<EOF > "$DEVPREFIX/bin/jhbuild"
 #!$DEVPREFIX/bin/bash
 export DEVROOT="$DEVROOT"
 export DEVPREFIX="$DEVPREFIX"
-export PYTHONPATH="$PYTHONPATH"
+export PYTHONUSERBASE="$PYTHONUSERBASE"
 export PIPENV_DOTENV_LOCATION="$DEVPREFIX/etc/pipenv-env"
 export PIPENV_PIPFILE="$DEVPREFIX/etc/Pipfile"
 export PYENV_ROOT="$PYENV_ROOT"
+export PYENV_PYTHON_VERSION="$PYENV_PYTHON_VERSION"
+export PATH="$PYENV_ROOT/shims:\$PATH"
+export CARGO_HOME="$CARGO_HOME"
 export RUSTUP_HOME="$RUSTUP_HOME"
+export WORKON_HOME="$WORKON_HOME"
 
 exec $DEVPREFIX/bin/pipenv run jhbuild \$@
 EOF
@@ -216,9 +231,6 @@ if test "x`echo $PATH | grep "$DEVPREFIX/bin"`" == x ; then
     echo "PATH does not contain $DEVPREFIX/bin. You probably want to fix that."
     export PATH="$DEVPREFIX/bin:$PATH"
 fi
-# pipenv wants enum34 because it's installed with Py2 but that conflicts
-# with Py3 so remove it.
-pip_remove enum34
 
 SDKROOT=`xcrun --show-sdk-path`
 
@@ -229,7 +241,6 @@ export PATH="$PYENV_ROOT/shims:$DEVPREFIX/bin:$PYENV_INSTALL_ROOT/plugins/python
 if test -d "$SDKROOT"; then
     export CFLAGS="-isysroot $SDKROOT -I$SDKROOT/usr/include"
 fi
-export PYTHON_CONFIGURE_OPTS="--enable-shared"
 
 $PIPENV install
 
